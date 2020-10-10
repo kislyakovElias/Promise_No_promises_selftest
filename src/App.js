189c8ee4316b26a56217232ec20b9f7a1a151678
superfoxx <super_fox@mail.ru>
2020-10-10 00:51:11 -0700

    smth new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Lists from "./Lists";
+import Buttons from "./Buttons";
+
+const initialCount = [
+    {id: 1, title: 'First', task:'Read a lable', status: false},
+    {id: 2, title: 'Second', task:'Drink a sable', status: false},
+    {id: 3, title: 'Third', task:'Burn a table', status: false},
+  ];
 
 function App() {
+
+  const [count, setCount] = useState(initialCount);
+
+  const Del = (id) => {
+   const newList = count.filter(el => el.id !== id);
+   setCount(newList)
+  };
+
+  const statusUpdate = (id) => {
+    const updList = count.map(el => {
+        if (el.id === id) return {...el, status: !el.status};
+        else return el;
+    })
+      setCount(updList);
+  };
+
+  const addNew = (addTitle, addTask) => {
+      const newList = [...count,{id: Math.random(),
+                                title: addTitle,
+                                task:addTask,
+                                status: false}];
+
+      setCount(newList);
+  };
+
+  const editCount = (addTitle, addTask, id) => {
+      const newList = count.map(el => {
+          if (el.id === id) return {...el, title:addTitle, task:addTask}
+      return el
+      })
+  setCount(newList);
+  }
+
+  const moveHandler = (currentIndex, nextIndex) => {
+    const newList = [...count];
+    const element = newList[currentIndex];
+    newList[currentIndex] = newList[nextIndex];
+    newList[nextIndex] = element;
+    setCount(newList);
+
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+
+        <Buttons addNew={addNew}/>
+        {count.map((el, index) => <Lists stUpd={statusUpdate} Del={Del} el={el} index={index} isLast ={index === count.length-1} moveHandler={moveHandler} editCount={editCount}/>)}
+
     </div>
   );
 }
